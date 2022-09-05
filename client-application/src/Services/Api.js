99f4ed78e58171a796df7f442b028d1b44d7c435
Mrityunjay383 <mrityunjayvyasmhl@gmail.com>
2022-09-05 16:56:15 +0530

    Bug Resolved

--- client-application/src/Services/Api.js
+++ client-application/src/Services/Api.js
@@ -14,15 +14,15 @@ import axios from "axios";
  export const callAssignedChats= async(baseChatSystemURL)=>{
   return await axios.get(`${baseChatSystemURL}/assigned`, { validateStatus: false, withCredentials: true }).then((response) => {
     return response.data.assignList;
-    });   
+    });
   };
- 
+
   export const callNoPendingChats =async (baseChatSystemURL)=>{
     return await axios.get(`${baseChatSystemURL}/noOfPendingTemplates `, { validateStatus: false, withCredentials: true }).then((response) => {
       return response.data.noOfPendingTemplates;
     });
   }
- 
+
   export const callgetflows =async(baseBulkMessagingURL,userId)=>{
     return await axios.post(`${baseBulkMessagingURL}/getflows`, {managerId: userId}, { validateStatus: false, withCredentials: true }).then((response) => {
       //setting the templates with the response from the API
@@ -41,11 +41,11 @@ import axios from "axios";
       //setting the optedinUsers with the response from the API
       return response.data.users;
     });
-    
+
   }
-  
+
   export const callstoredCustomers =async (baseBulkMessagingURL)=>{
-    
+
     return await axios.get(`${baseBulkMessagingURL}/storedCustomers`, { validateStatus: false, withCredentials: true }).then((response) => {
       //getting the stored users from the response from the API
 return response.data.users;
@@ -55,7 +55,7 @@ return response.data.users;
   export const callbroadcastMessage =async (baseBulkMessagingURL,message,toBeBroadcastNo,userId)=>{
     return await axios.post(`${baseBulkMessagingURL}/broadcastMessage`, {message, toBeBroadcastNo, userId}, {validateStatus: false, withCredentials: true}).then((response) => {
       return response.data;
-      
+
     });
   }
 
@@ -91,21 +91,21 @@ return response.data.users;
     });
   }
   export const callLogin=async(baseURL,email,password)=>{
-    axios.post(`${baseURL}/auth/login`, {email, password}, {validateStatus: false, withCredentials: true}).then((response) => {
+    return await axios.post(`${baseURL}/auth/login`, {email, password}, {validateStatus: false, withCredentials: true}).then((response) => {
       if(response.status === 200 && response.data.success){
         console.log("User Logedin");
         return response.data.user;
       }else{
         console.log("Login Failed");
-      return false;    }
-
+        return false;
+      }
     });
   }
 
   export const callindiuser=async(baseURL,id)=>{
     return await axios.post(`${baseURL}/indi_user`, {userId: id}, { validateStatus: false, withCredentials: true }).then((response) => {
       return response.data.foundUser;
-     
+
     });
   }
 
